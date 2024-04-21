4f281adfa919e49667fda472ae1ce7db32505aa2
Andrew Porter <partydrone@icloud.com>
2024-04-21 11:01:58 -0500

    Add support for lvh.me host
    This makes it easy to test subdomain routing.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -70,4 +70,7 @@ Rails.application.configure do
 
   # Raise error when a before_action's only/except options reference missing actions
   config.action_controller.raise_on_missing_callback_actions = true
+
+  # Support lvh.me to test subdomains in development
+  config.hosts << "lvh.me:3000"
 end
