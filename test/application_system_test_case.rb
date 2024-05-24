295653ae339c64f7e269e40e1ba93a9345e691da
Andrew Porter <partydrone@icloud.com>
2024-05-24 13:21:18 -0500

    Configure system tests to use Selenium

--- test/application_system_test_case.rb
+++ test/application_system_test_case.rb
@@ -1,5 +1,18 @@
 require "test_helper"
 
 class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
-  driven_by :selenium, using: :chrome, screen_size: [1400, 1400]
+  def setup
+    Capybara.server_host = "0.0.0.0"
+    Capybara.app_host = "http://#{ IPSocket.getaddress(Socket.gethostname) }" if ENV["SELENIUM_REMOTE_URL"].present?
+    super
+  end
+
+  url = ENV.fetch("SELENIUM_REMOTE_URL", nil)
+  options = if url
+    {browser: :remote, url: url}
+  else
+    {browser: :chrome}
+  end
+
+  driven_by :selenium, using: :headless_chrome, options: options, screen_size: [1400, 1400]
 end
