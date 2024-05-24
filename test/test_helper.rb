5f3835f694d34ba5fd7a4b234bca9f663e3bae90
Andrew Porter <partydrone@icloud.com>
2024-05-24 13:20:35 -0500

    Add minitest-rails to test_helper

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -34,6 +34,7 @@ SimpleCov.formatters = SimpleCov::Formatter::MultiFormatter.new([
 ENV["RAILS_ENV"] ||= "test"
 require_relative "../config/environment"
 require "rails/test_help"
+require "minitest/rails"
 require "minitest/reporters"
 
 Minitest::Reporters.use! [Minitest::Reporters::DefaultReporter.new(color: true)]
